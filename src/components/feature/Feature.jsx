d907d49866f3ef6699f080aef4f87ef1886dc49f
Ikenna Akano <95446875+ikennaex@users.noreply.github.com>
2023-03-04 08:35:16 +0100

    Update Feature.jsx

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -4,9 +4,9 @@ import "./feature.css"
 const Feature = ({title, text}) => {
   return (
     <div className='gpt3-features-container-feature'>
-
       <div className='gpt3-features-container-feature-title'>
         <div />
+    
         <h1>{title}</h1>
       </div>
 

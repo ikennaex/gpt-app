ff01a2de3c7d6fd5831373e8201ecc85e1326b7b
Ikenna Akano <95446875+ikennaex@users.noreply.github.com>
2023-03-04 08:34:43 +0100

    Update Article.jsx

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -10,6 +10,7 @@ const Article = ({imgUrl, date, title}) => {
 
       <div className='gpt3-blog-container-article-content'>
         <div>
+        
           <p>{date}</p>
           <h3>{title}</h3>
         </div>
